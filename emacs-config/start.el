5c29b247612ad94daa63bfe0dc4e1c0e6880ba3d
Kanasansoft <kanasansoft@gmail.com>
2010-02-28 21:46:24 +0900

    Emacsの起動時間を測定する処理を追加。

--- emacs-config/start.el
+++ emacs-config/start.el
@@ -1,6 +1,19 @@
 ;;; start.el
 ;;;
 
+;;;起動時間を測定する(前処理)
+(defun my-time-lag ()
+  (let* ((now (current-time))
+         (min (- (car now) (car my-time-zero)))
+         (sec (- (car (cdr now)) (car (cdr my-time-zero))))
+         (msec (/ (- (car (cdr (cdr now)))
+                     (car (cdr (cdr my-time-zero))))
+                     1000))
+         (lag (+ (* 60000 min) (* 1000 sec) msec)))
+    (message "'.start.el' loading time: %d msec." lag)))
+;;;起動時間を測定する(開始)
+(setq my-time-zero (current-time))
+
 (defvar my-emacs-config-dir (expand-file-name "~/dotfiles/emacs-config/myconfig"))
 
 (defun my-emacs-load-config (name)
@@ -70,3 +83,5 @@ Assume that the filename should be config-NAME.el."
 ;(setq Info-directory-list '( "~/share/info" "/usr/local/share/info" 
 ;                             "/sw/share/info" "/usr/share/info"))
 
+;;;起動時間を測定する(終了)
+(my-time-lag)
