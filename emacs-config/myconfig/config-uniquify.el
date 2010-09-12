a7c5deabacfc42af9935db6c113f72ddb25a3756
Kanasansoft <kanasansoft@gmail.com>
2010-09-12 13:51:06 +0900

    ファイル名がかぶった場合にバッファ名をわかりやすくする

---
+++ emacs-config/myconfig/config-uniquify.el
@@ -0,0 +1,7 @@
+;;;add 2010/09/12
+;;;ファイル名がかぶった場合にバッファ名をわかりやすくする
+(require 'uniquify)
+;;filename<dir> 形式のバッファ名にする
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+;;*で囲まれたバッファ名は対象外にする
+(setq uniquify-ignore-buffers-re "*[^*]+*")
