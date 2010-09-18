811b5a465887e804d72e3b553ae30e69b8df9487
Kanasansoft <kanasansoft@gmail.com>
2010-09-19 01:14:22 +0900

    矩形選択を行ないやすくするsense-region.elを追加。

---
+++ emacs-config/myconfig/config-sense-region.el
@@ -0,0 +1,4 @@
+;; (auto-install-from-url "http://taiyaki.org/elisp/sense-region/src/sense-region.el")
+(autoload 'sense-region-on "sense-region"
+  "System to toggle region and rectangle." t nil)
+(sense-region-on)
