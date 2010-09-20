ac3cb8978fef9598b1a8c1cf8774379f1591eab6
Kanasansoft <kanasansoft@gmail.com>
2010-09-21 01:05:13 +0900

    hippie-expandの設定を追加。

---
+++ emacs-config/myconfig/config-hippie-expand.el
@@ -0,0 +1,12 @@
+(setq hippie-expand-try-functions-list
+      '(yas/hippie-try-expand
+        try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-all-abbrevs
+        try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol
+        try-expand-list
+        try-expand-line))
