8fb3155d729a24830e5f4e8cc3d9596654e8fde3
Kanasansoft <kanasansoft@gmail.com>
2010-10-05 02:22:22 +0900

    junk-fileの設定を追加。

---
+++ emacs-config/myconfig/config-open-junk-file.el
@@ -0,0 +1,5 @@
+;; (auto-install-from-emacswiki "open-junk-file.el")
+;; (dired-create-directory "~/dotfiles/emacs-config/junk")
+(require 'open-junk-file)
+(setq open-junk-file-format "~/dotfiles/emacs-config/junk/%Y-%m-%d-%H%M%S.")
+(global-set-key (kbd "C-z j") 'open-junk-file)
