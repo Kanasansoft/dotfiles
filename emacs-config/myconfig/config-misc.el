6b518a38f0e5b1c27aee3ec09b3f7891c1a1b5fc
Kanasansoft <kanasansoft@gmail.com>
2010-10-07 01:52:28 +0900

    proxyの設定を独立させた。

--- emacs-config/myconfig/config-misc.el
+++ emacs-config/myconfig/config-misc.el
@@ -54,9 +54,3 @@
 ;;;add 2010/10/03
 ;;;読み込み専用ファイルをview-modeで開くようにする
 (setq view-read-only t)
-
-;;;add 2010/10/05
-;;;プロキシの設定
-;(setq url-proxy-services '(("no_proxy" . "www\\.regexp\\.sample\\.com")
-;                           ("http"     . "proxy.server:8080")
-;                           ("ftp"      . "proxy.server:8080")))
---
+++ emacs-config/myconfig/config-proxy.el
@@ -0,0 +1,5 @@
+;;;add 2010/10/05
+;;;プロキシの設定
+;(setq url-proxy-services '(("no_proxy" . "www\\.regexp\\.sample\\.com")
+;                           ("http"     . "proxy.server:8080")
+;                           ("ftp"      . "proxy.server:8080")))
