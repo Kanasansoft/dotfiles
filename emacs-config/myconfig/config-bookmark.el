0a54c35551778c9bfb84481fb40952cef5f4a21f
Kanasansoft <kanasansoft@gmail.com>
2010-10-07 02:08:55 +0900

    bookmark.elの設定を追加。

---
+++ emacs-config/myconfig/config-bookmark.el
@@ -0,0 +1,10 @@
+;; ブックマークを変更したら即保存する
+(setq bookmark-save-flag 1)
+;; 超整理法(好みに応じて)
+(progn
+  (setq bookmark-sort-glag nil)
+  (defun bookmark-arrange-latest-top ()
+    (let ((latest (bookmark-get-bookmark bookmark)))
+      (setq bookmark-alist (cons latest (delq latest bookmark-alist))))
+    (bookmark-save))
+  (add-hook 'bookmark-after-jump-hook 'bookmark-arrange-latest-top))
