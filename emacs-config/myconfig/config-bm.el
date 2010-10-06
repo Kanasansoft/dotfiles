5fdcda56ea7457762c8732d517824cddc7b9fe14
Kanasansoft <kanasansoft@gmail.com>
2010-10-07 02:46:15 +0900

    bm.elの設定を追加。

---
+++ emacs-config/myconfig/config-bm.el
@@ -0,0 +1,13 @@
+;; (install-elisp "http://cvs.savannah.gnu.org/viewvc/*checkout*/bm/bm/bm.el")
+
+(setq-default bm-buffer-persistence nil)
+(setq bm-restore-repository-on-load t)
+(require 'bm)
+(add-hook 'find-file-hooks   'bm-buffer-restore)
+(add-hook 'kill-buffer-hook  'bm-buffer-save)
+(add-hook 'after-save-hook   'bm-buffer-save)
+(add-hook 'after-revert-hook 'bm-buffer-restore)
+(add-hook 'vc-before-checkin-book 'bm-buffer-save)
+(global-set-key (kbd "M-SPC") 'bm-toggle)
+(global-set-key (kbd "M-[") 'bm-previous)
+(global-set-key (kbd "M-]") 'bm-next)
