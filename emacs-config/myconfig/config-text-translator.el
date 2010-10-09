0b3c23b00fcc9fe7b95e25983529d5812828cc93
Kanasansoft <kanasansoft@gmail.com>
2010-10-09 18:30:02 +0900

    EmacsからWeb上の翻訳サービスを使えるようにした。

---
+++ emacs-config/myconfig/config-text-translator.el
@@ -0,0 +1,7 @@
+;; (auto-install-batch "text translator")
+;;from Emacsテクニックバイブル
+;;言語を自動判別して複数のWeb翻訳サービスを同時に使う
+(require 'text-translator)
+(setq text-translator-auto-selection-func
+      'text-translator-translate-by-auto-selection-enja)
+(global-set-key (kbd "C-z t") 'text-translator-all-by-auto-selection)
