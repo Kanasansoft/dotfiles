f0531c26a1ea2a9539d7bb0b81c1a80fb3a63261
Kanasansoft <kanasansoft@gmail.com>
2010-10-10 02:31:58 +0900

    cygwinやGnuWin32等のバイナリをemacsから利用できるようにした。

---
+++ emacs-config/myconfig/config-linux-like-environment.el
@@ -0,0 +1,10 @@
+;;windows only stuff
+;;http://stackoverflow.com/questions/2169520/how-to-get-grep-working-in-emacs-on-windows-emacsw32
+;;cygwin等のバイナリをemacsから利用できるようにする
+;;cygwinやGnuWin32のパスの設定はconfig-variable.elで行なっている
+(when (string-equal system-type "windows-nt")
+(progn
+(setenv "PATH"
+(concat cygwin-bin-path ";" gnu-bin-path ";"))
+(setq exec-path
+'(cygwin-bin-path gnu-bin-path))))
