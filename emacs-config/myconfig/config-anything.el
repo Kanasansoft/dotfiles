985ac4a3c0d46eee62a6e13852217f6635e774e4
Kanasansoft <kanasansoft@gmail.com>
2010-11-03 00:28:31 +0900

    anythingの1情報源に表示される最大候補数を変更。

--- emacs-config/myconfig/config-anything.el
+++ emacs-config/myconfig/config-anything.el
@@ -3,6 +3,7 @@
 (require 'anything-startup)
 (define-key anything-map (kbd "C-h") nil)
 (global-set-key (kbd "M-y") 'anything-show-kill-ring)
+(setq anything-candidate-number-limit 100)
 
 ;;; (auto-install-from-emacswiki "anything.el")
 ;;; (auto-install-from-emacswiki "anything-config.el")
