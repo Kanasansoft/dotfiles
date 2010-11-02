bb12714a315c929a1798f4f862d5613bad4ebfd5
Kanasansoft <kanasansoft@gmail.com>
2010-11-03 00:27:41 +0900

    anything-show-kill-ring にキーバインドを設定。

--- emacs-config/myconfig/config-anything.el
+++ emacs-config/myconfig/config-anything.el
@@ -2,6 +2,7 @@
 ;;from Emacsテクニックバイブル
 (require 'anything-startup)
 (define-key anything-map (kbd "C-h") nil)
+(global-set-key (kbd "M-y") 'anything-show-kill-ring)
 
 ;;; (auto-install-from-emacswiki "anything.el")
 ;;; (auto-install-from-emacswiki "anything-config.el")
