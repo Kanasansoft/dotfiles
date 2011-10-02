2aa10215f3c7e9e161bf9a133f339a1f91b87f90
Kanasansoft <kanasansoft@gmail.com>
2011-10-03 01:48:05 +0900

    文字コードを表示するelispの設定をいじった。
     * 読み込まれていなかったのを修正した。
     * グローバルで利用できるようにした。
     * bufferを開いた時に有効になるようにした。

--- emacs-config/myconfig/config-what-char.el
+++ emacs-config/myconfig/config-what-char.el
@@ -1,2 +1,7 @@
 ;; (auto-install-from-url "http://homepage3.nifty.com/satomii/software/what-char.el")
 
+(require 'what-char)
+(what-char-add-to-mode-line)
+(define-globalized-minor-mode global-what-char-mode
+  what-char-mode what-char-mode)
+(global-what-char-mode t)
